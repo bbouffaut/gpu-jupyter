0541ef69477fcc9f45dae0768ecfc2d891009676
Baptiste Bouffaut <baptiste.bouffaut@gmail.com>
2020-09-25 13:46:15 +0200

    change docker restart policy

--- start_docker_registry.sh
+++ start_docker_registry.sh
@@ -1,3 +1,2 @@
 #!/bin/sh
-docker run -d -it -p 8888:8888 -v $(pwd)/data:/home/jovyan/work -e GRANT_SUDO=yes -e JUPYTER_ENABLE_LAB=yes -e NVIDIA_VISIBLE_DEVICES=0 --user root --restart on-failure --name gpu-jupyter registry.bb
-ofamily.com/gpu-jupyter_cuda9.0:latest
+docker run -d -it -p 8888:8888 -v $(pwd)/data:/home/jovyan/work -e GRANT_SUDO=yes -e JUPYTER_ENABLE_LAB=yes -e NVIDIA_VISIBLE_DEVICES=0 --user root --restart unless-stopped --name gpu-jupyter registry.bbofamily.com/gpu-jupyter_cuda9.0:1.1
