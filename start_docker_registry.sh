8b21b0b8c9463fd0553640baba667cebf2a10ff3
Baptiste Bouffaut <baptiste.bouffaut@gmail.com>
2020-12-14 08:43:56 +0100

    various config changes

--- start_docker_registry.sh
+++ start_docker_registry.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-docker run -d -it -p 8888:8888 -v /srv/1-workspace/notebooks:/home/jovyan/work -e GRANT_SUDO=yes -e JUPYTER_ENABLE_LAB=yes -e NVIDIA_VISIBLE_DEVICES=0 --user root --restart unless-stopped --name gpu-jupyter registry.bbofamily.com/gpu-jupyter_cuda9.0:1.1
+docker run -d -it -p 8888:8888 -v /srv/1-workspace/notebooks:/home/jovyan/work -e GRANT_SUDO=yes -e JUPYTER_ENABLE_LAB=yes -e NVIDIA_VISIBLE_DEVICES=0 --restart unless-stopped --user root --name gpu-jupyter registry.bbofamily.com/gpu-jupyter_cuda9.0:1.1
